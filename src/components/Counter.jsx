6226b9a007122f8fe2bf1de1d9732c8fb475d1bd
shokrullah2023 <shokrullahakhlaqi01@gmail.com>
2025-03-03 21:05:12 -0700

    Day 3 is completed

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function Counter(){
-    const [count, setCount]= useState(0);
-
-    return (
-        <div style={ {textAlign: "center", marginTop:"50px"}}>
-            <h1>Counter: {count}</h1>
-            <button onClick={()=>setCount(count+1)}>Increment</button>
-            <button onClick={()=> setCount(count-1)}>Decrement</button>
-        </div>
-    );
+function Counter({ count, setCount }) {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
 }
 
 export default Counter;
--- src/components/PropsPassing.jsx
+++ src/components/PropsPassing.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
 
-function Child({}){
-
+function Child({ updateMessage }) {
+  return (
+    <button onClick={() => updateMessage("Hello from Child")}>Click Me</button>
+  );
 }
+
+function Parent() {
+  const [message, setMessage] = useState("Hello from Parent");
+
+  return (
+    <div>
+      <h2>{message}</h2>
+      <Child updateMessage={setMessage} />
+    </div>
+  );
+}
+
+export default Parent;
